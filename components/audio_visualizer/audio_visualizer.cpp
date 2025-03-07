0f78e2453d1031ca643fe4a30e00e99a83f4d604
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-07 23:27:27 +0100

    Update audio_visualizer.cpp

--- components/audio_visualizer/audio_visualizer.cpp
+++ components/audio_visualizer/audio_visualizer.cpp
@@ -3,7 +3,7 @@
 namespace esphome {
 namespace audio_visualizer {
 
-AudioVisualizer::AudioVisualizer() {}
+AudioVisualizer::AudioVisualizer() : PollingComponent(20ms) {}
 
 void AudioVisualizer::setup() {
   lv_obj_t *obj = this->create_widget();
@@ -14,7 +14,7 @@ void AudioVisualizer::setup() {
 
   // Initialiser les données avec des valeurs aléatoires
   for (int i = 0; i < 64; ++i) {
-    data[i] = rand() % this->height_;
+    data[i] = rand() % (this->height_ / 2) + (this->height_ / 2); // Initialisation haute
   }
 }
 
@@ -25,47 +25,41 @@ lv_obj_t *AudioVisualizer::create_widget() {
 void AudioVisualizer::update() {
   // Mettre à jour les données avec des valeurs aléatoires
   for (int i = 0; i < 64; ++i) {
-    // Générer une nouvelle valeur, mais l'adoucir un peu
-    int change = (rand() % 21) - 10; // Valeur aléatoire entre -10 et 10
+    int change = (rand() % 11) - 5; // Moins de changement
     data[i] += change;
 
-    // Limiter les valeurs pour qu'elles restent dans la plage valide
-    if (data[i] < 0) data[i] = 0;
-    if (data[i] > this->height_) data[i] = this->height_;
+    if (data[i] < this->height_ / 2) data[i] = this->height_ / 2; // Limite basse
+    if (data[i] > this->height_) data[i] = this->height_; // Limite haute
   }
-  lv_obj_invalidate(this->obj_); // Force le redessin
+  lv_obj_invalidate(this->obj_);
 }
 
 void AudioVisualizer::draw() {
-  // Dessiner les barres en utilisant les données aléatoires
-  int num_bars = 64; // Nombre de barres (correspond à la taille du tableau `data`)
-  int bar_width = this->width_ / num_bars;
-  int spacing = 0; // Pas d'espacement entre les barres pour ressembler à l'image
+  int num_bars = 64;
+  int bar_width = (this->width_ - num_bars + 1) / num_bars;
+  int spacing = 1;
 
   for (int i = 0; i < num_bars; ++i) {
-    // Calculer les coordonnées de la barre
     lv_coord_t x1 = i * (bar_width + spacing);
-    lv_coord_t y1 = this->height_ - data[i]; // On part du bas
+    lv_coord_t y1 = this->height_ - data[i];
     lv_coord_t x2 = x1 + bar_width;
     lv_coord_t y2 = this->height_;
 
-    // Définir le style de la barre (dégradé de couleur)
     lv_draw_rect_dsc_t rect_dsc;
     lv_draw_rect_dsc_init(&rect_dsc);
 
-    // Créer un dégradé de couleur (bleu -> violet -> rouge)
+    // Dégradé de couleur (bleu -> violet -> rouge)
     lv_color_t color;
-    if (i < num_bars / 2) {
-      // Bleu -> Violet
-      color = lv_color_mix(LV_COLOR_BLUE, LV_COLOR_PURPLE, (i * 255) / (num_bars));
+    if (i < num_bars / 3) {
+      color = lv_color_mix(LV_COLOR_BLUE, LV_COLOR_PURPLE, (i * 255) / (num_bars / 3));
+    } else if (i < 2 * num_bars / 3) {
+      color = lv_color_mix(LV_COLOR_PURPLE, LV_COLOR_RED, ((i - num_bars / 3) * 255) / (num_bars / 3));
     } else {
-      // Violet -> Rouge
-      color = lv_color_mix(LV_COLOR_PURPLE, LV_COLOR_RED, ((i - num_bars / 2) * 255) / (num_bars));
+      color = lv_color_mix(LV_COLOR_RED, LV_COLOR_WHITE, ((i - 2 * num_bars / 3) * 255) / (num_bars / 3)); // Rouge -> Blanc
     }
     rect_dsc.bg_color = color;
-    rect_dsc.radius = 0; // Pas d'arrondi
+    rect_dsc.radius = 0;
 
-    // Dessiner la barre
     lv_area_t coords;
     coords.x1 = x1;
     coords.y1 = y1;
@@ -73,8 +67,23 @@ void AudioVisualizer::draw() {
     coords.y2 = y2;
 
     lv_draw_rect(&coords, &rect_dsc);
+
+    // Points blancs
+    if (rand() % 15 == 0) { // Moins de points
+      lv_draw_rect_dsc_t point_dsc;
+      lv_draw_rect_dsc_init(&point_dsc);
+      point_dsc.bg_color = LV_COLOR_WHITE;
+      point_dsc.radius = LV_RADIUS_CIRCLE;
+      lv_area_t point_coords;
+      point_coords.x1 = x1 + rand() % bar_width;
+      point_coords.y1 = y1 - (rand() % (this->height_ / 10) + 3); // Plus près des barres
+      point_coords.x2 = point_coords.x1 + 2; // Plus petits
+      point_coords.y2 = point_coords.y1 + 2;
+      lv_draw_rect(&point_coords, &point_dsc);
+    }
   }
 }
 
 }  // namespace audio_visualizer
 }  // namespace esphome
+
